f952dac914fe4ed93543c7b4a9b883e3752bfe72
sungsoos <choiseungsoos@gmail.com>
2025-06-13 10:52:37 +0900

    ico_milkt.js

---
+++ ico_milkt.js
@@ -0,0 +1,1 @@
+document.head.innerHTML += '<style>.ico.doneToday, .ico.doneTest {background: url(https://www.shareicon.net/data/64x64/2016/08/20/817720_check_395x512.png) no-repeat center/150% !important;}</style>'
