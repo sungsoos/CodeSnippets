be6e1268e0179b43b6cfba751b3ccaa9a17f49cc
승수 <choiseungsoos@gmail.com>
2025-06-16 08:33:48 +0900

    RemoveThings.js

---
+++ RemoveThings.js
@@ -0,0 +1,5 @@
+if (document.querySelector(".update") != null) {
+    document.querySelector(".otherBnr").innerHTML = ""; // remove ad
+    document.querySelector(".event").innerHTML = ""; // remove event
+    document.querySelector(".update").innerHTML = ""; // remove update
+}
