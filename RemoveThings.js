cd12708eb6147ebff5354e2585aedc90d75bab2a
승수 <choiseungsoos@gmail.com>
2025-06-16 08:55:56 +0900

    Update

--- RemoveThings.js
+++
@@ -1,5 +0,0 @@
-if (document.querySelector(".update") != null) {
-    document.querySelector(".otherBnr").innerHTML = ""; // remove ad
-    document.querySelector(".event").innerHTML = ""; // remove event
-    document.querySelector(".update").innerHTML = ""; // remove update
-}
---
+++ RemoveThings_milkt.js
@@ -0,0 +1,9 @@
+if (document.querySelector(".update") != null) {
+    document.querySelector(".otherBnr").remove(); // remove ad
+    document.querySelector(".event").remove(); // remove event
+    document.querySelector(".update").remove(); // remove update
+
+    document.querySelector(".ingEvnt").remove(); // remove update on top
+    document.querySelector(".myPlan").style.width = "50%";
+    document.querySelector(".myProfile").style.width = "50%";
+}
